b69a304cbdc00fd66b284f48520c5ba0296cb851
Волейко Матвей Викторович <voleikomatvey@gmail.com>
2026-05-17 07:44:48 +0000

    add ex[anded describtion

--- backend/models/models.go
+++ backend/models/models.go
@@ -9,15 +9,16 @@ const (
 )
 
 type Product struct {
-	ID           uint         `json:"id" gorm:"primaryKey"`
-	Title        string       `json:"title" gorm:"type:varchar(255);not null" binding:"required"`
-	Description  string       `json:"description" gorm:"type:text"`
-	Category     string       `json:"category" gorm:"type:varchar(100);index" binding:"required"`
-	Price        float64      `json:"price" gorm:"type:numeric(15,2);index" binding:"required"`
-	PremiumLevel PremiumLevel `json:"premium_level" gorm:"type:varchar(50);index"`
-	ImageKey     string       `json:"image_key" gorm:"type:varchar(255)"`
-	ImageURL     string       `json:"image_url" gorm:"-"`
-	Rating       float32      `json:"rating" gorm:"type:real;default:0"`
+	ID                  uint         `json:"id" gorm:"primaryKey"`
+	Title               string       `json:"title" gorm:"type:varchar(255);not null" binding:"required"`
+	Description         string       `json:"description" gorm:"type:text"`
+	ExpandedDescription string       `json:"expanded_description" gorm:"type:text"`
+	Category            string       `json:"category" gorm:"type:varchar(100);index" binding:"required"`
+	Price               float64      `json:"price" gorm:"type:numeric(15,2);index" binding:"required"`
+	PremiumLevel        PremiumLevel `json:"premium_level" gorm:"type:varchar(50);index"`
+	ImageKey            string       `json:"image_key" gorm:"type:varchar(255)"`
+	ImageURL            string       `json:"image_url" gorm:"-"`
+	Rating              float32      `json:"rating" gorm:"type:real;default:0"`
 }
 
 type CartItem struct {
